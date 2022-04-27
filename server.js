7341012723faed79518e98cd10bf3dee5eafe1c5
Ivan <adamovskij00@mail.ru>
2022-04-27 21:56:52 +0300

    add fix server.js

--- server.js
+++ server.js
@@ -22,17 +22,17 @@ app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`)
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({extended: false}));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.use(express.static('styles'));
 
-app.use(methodOverride('__method'));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   const title = 'Home';
-  res.render(createPath('index'), {title});
+  res.render(createPath('index'), { title });
 });
 
 app.get('/contacts', (req, res) => {
@@ -49,62 +49,63 @@ app.get('/posts/:id', (req, res) => {
   const title = 'Post';
   Post
     .findById(req.params.id)
-    .then((posts) => res.render(createPath('post'), {title, post}))
+    .then(post => res.render(createPath('post'), {post, title}))
     .catch((error) => {
       console.log(error);
-      res.render(createPath('error'), {title: 'Error'})
+      res.render(createPath('error'), {title: 'Error'});
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.sendStatus(200);
     })
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), {title: 'Error'});
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('edit-post'), {post, title}))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), {title: 'Error'});
+    });
 });
 
 app.put('/edit/:id', (req, res) => {
   const {title, author, text} = req.body;
   const {id} = req.params;
-
   Post
     .findByIdAndUpdate(id, {title, author, text})
-    .then(result => res.redirect('/posts/${id}'))
+    .then((result) => res.redirect(`/posts/${id}`))
     .catch((error) => {
       console.log(error);
-      res.render(createPath('error'), {title: 'Error'})
-    })
+      res.render(createPath('error'), {title: 'Error'});
+    });
 });
-
-app.delete('/post/:id', (req, res) => {
-  const title = 'Post';
-  Post
-    .findByIdAndDelete(req.params.id)
-    .then(result => {
-      res.sendStatus(200)
-    })
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), {title: "Error"});
-    })
-})
-
-app.get('/edit/:id', (req, res) => {
-  const title = 'Edit post';
-  Post
-    .findById(req.params.id)
-    .then(post => {
-      res.render(createPath('edit-post'), {post, title})
-    })
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), {title: "Error"});
-    })
-})
 
 app.get('/posts', (req, res) => {
   const title = 'Posts';
   Post
     .find()
     .sort({createdAt: -1})
-    .then((posts) => res.render(createPath('posts'), {title, posts}))
+    .then(posts => res.render(createPath('posts'), {posts, title}))
     .catch((error) => {
       console.log(error);
-      res.render(createPath('error'), {title: 'Error'})
-    })
+      res.render(createPath('error'), {title: 'Error'});
+    });
+});
+
+app.get('/add-post', (req, res) => {
+  const title = 'Add Post';
+  res.render(createPath('add-post'), {title});
 });
 
 app.post('/add-post', (req, res) => {
@@ -116,12 +117,7 @@ app.post('/add-post', (req, res) => {
     .catch((error) => {
       console.log(error);
       res.render(createPath('error'), {title: 'Error'});
-    })
-});
-
-app.get('/add-post', (req, res) => {
-  const title = 'Add Post';
-  res.render(createPath('add-post'), {title});
+    });
 });
 
 app.use((req, res) => {
