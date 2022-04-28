d5ce54f403f5c189449da6cf52a593be7bc94179
Ivan <adamovskij00@mail.ru>
2022-04-28 08:33:22 +0300

    add contact-routes requests

---
+++ routes/contacts-routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/contacts', (req, res) => {
+  const title = 'Contacts';
+  const contacts = [
+    {name: 'YouTube', link: 'http://youtube.com/aivan3d66'},
+    {name: 'Linkedin', link: 'https://www.linkedin.com/in/ivan-adamouski-55a421227/'},
+    {name: 'GitHub', link: 'http://github.com/aivan3d66'},
+  ];
+  res.render(createPath('contacts'), {contacts, title});
+});
+
+module.exports = router;
